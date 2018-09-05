b4ebf8174694799b19eaa5a946ba7f00827eeef5
Samuel Verschelde <stormi@laposte.net>
2018-09-05 10:59:45 +0200

    Change checkbox text for the EXT vs LVM option
    This commit is the version that the initial installer for XCP-ng 7.5.0
    had.

--- tui/installer/screens.py
+++ tui/installer/screens.py
@@ -690,9 +690,7 @@ def select_guest_disks(answers):
     cbt = CheckboxTree(3, scroll)
     for (c_text, c_item) in entries:
         cbt.append(c_text, c_item, c_item in currently_selected)
-    txt = "Enable thin provisioning"
-    if len(BRAND_VDI) > 0:
-        txt += " (Optimized storage for %s)" % BRAND_VDI
+    txt = "Use EXT instead of LVM (leave unchecked if unsure)"
     tb = Checkbox(txt, srtype == constants.SR_TYPE_EXT and 1 or 0)
     
     gf = GridFormHelp(tui.screen, 'Virtual Machine Storage', 'guestdisk:info', 1, 4)
