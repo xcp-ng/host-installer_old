b20e3033ecc1072f01f1b4cf619f3b2b952e592f
Samuel Verschelde <stormi@laposte.net>
2020-03-05 11:34:56 +0100

    Remove mention of Technical Support Representative in error messages

--- constants.py
+++ constants.py
@@ -70,13 +70,6 @@ def error_string(error, logname, with_hd):
         ERROR_STRING_KNOWN_ERROR: "An unrecoverable error has occurred.  The error was:\n\n%s\n"
     }
 
-    if version.PRODUCT_VERSION:
-        ERROR_STRINGS = {
-            ERROR_STRING_UNKNOWN_ERROR_WITH_HD: "An unrecoverable error has occurred.  The details of the error can be found in the log file, which has been written to /tmp/%s (and /root/%s on your hard disk if possible).\n\nPlease refer to your user guide or contact a Technical Support Representative for more details.",
-            ERROR_STRING_UNKNOWN_ERROR_WITHOUT_HD: "An unrecoverable error has occurred.  The details of the error can be found in the log file, which has been written to /tmp/%s.\n\nPlease refer to your user guide or contact a Technical Support Representative for more details.",
-            ERROR_STRING_KNOWN_ERROR: "An unrecoverable error has occurred.  The error was:\n\n%s\n\nPlease refer to your user guide, or contact a Technical Support Representative, for further details."
-            }
-
     if error == "":
         if with_hd:
             return ERROR_STRINGS[ERROR_STRING_UNKNOWN_ERROR_WITH_HD] % (logname, logname)
