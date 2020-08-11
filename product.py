f0479a7180318493852314059ca017d4c24f0d9c
BenjiReis <benjamin.reis@vates.fr>
2020-08-11 16:14:36 +0200

    feat: keep 'sched-gran' grub entry on upgrade (#21)
    If Xen's scheduling mode has been defined previously, we keep the value when upgrading.

--- product.py
+++ product.py
@@ -385,6 +385,11 @@ class ExistingInstallation:
             (dom0_mem, dom0_mem_min, dom0_mem_max) = xcp.dom0.parse_mem(dom0_mem_arg[0])
             if dom0_mem:
                 results['host-config']['dom0-mem'] = dom0_mem / 1024 / 1024
+
+            #   - sched-gran
+            sched_gran = next((x for x in xen_args if x.startswith('sched-gran=')), None)
+            if sched_gran:
+                results['host-config']['sched-gran'] = sched_gran
         except:
             pass
         self.unmount_boot()
